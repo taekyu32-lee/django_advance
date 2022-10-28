7c130b2dcf30f796f4ef12048db54443a4eec61e
taekyu32-lee <taekyu32@gmail.com>
2022-10-28 17:00:38 +0900

    Feat:create Article

---
+++ article/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+# Create your views here.
+class ArticleView(APIView):
+    def get(request):
+        pass
+    def post(request):
+        pass
--- django_advance/urls.py
+++ django_advance/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -7,4 +7,5 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/article/',include("article.urls")),
 ]
